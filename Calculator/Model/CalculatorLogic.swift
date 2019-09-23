c0a92a65c525a6f0cd1b4d6b466254ff9a7640dd
Francesca Koulikov <fkoulikov@illuminateed.net>
2019-09-23 16:49:56 -0400

    adding functionality to do calculation

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -11,12 +11,13 @@ import Foundation
 struct CalculatorLogic {
     
     private var number: Double?
+    private var intermediateCaculation: (n1: Double, calcMethod: String)?
     
     mutating func setNumber(_ number: Double){
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         
         if let n = number {
             if symbol == "+/-" {
@@ -29,7 +30,35 @@ struct CalculatorLogic {
             else if symbol == "AC" {
                 return 0
             }
+            else if symbol == "="{
+                return performTwoNumCalculation(n2: n)
+            }
+            else {
+                intermediateCaculation = (n1: n, calcMethod: symbol)
+            }
 
+        }
+        return nil
+    }
+    
+    private func performTwoNumCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCaculation?.n1,
+            let operation = intermediateCaculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "*":
+                return n1 * n2
+            case "-":
+                return n1 - n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases")
+            }
+            
+            
         }
         return nil
     }
