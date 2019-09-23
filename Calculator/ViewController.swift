146f3440fe47063f314dab5bb348e1837139857e
Francesca Koulikov <fkoulikov@illuminateed.net>
2019-09-23 15:43:07 -0400

    learning about access level, setting some calculation to our app

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue *= -1
+            }
+            else if calcMethod == "%" {
+                displayValue *= 0.01
+                
+            }
+            else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            }
+        }
+        
+        
+        
     
     }
 
@@ -24,7 +52,33 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber{
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }
+            else {
+                if numValue == "."{
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text! += numValue
+            }
+           
+        }
+        
+        
     
+    }
+    
+    //MARK - other methods
+    func appendValue(add: String){
+        displayLabel.text?.append(contentsOf: add)
     }
 
 }
